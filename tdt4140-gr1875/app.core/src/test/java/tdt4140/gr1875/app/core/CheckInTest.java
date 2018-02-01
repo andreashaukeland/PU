5d31c1c88719066740019567dc0497a475dec6ff
Andreas <andreas.haukeland.95@gmail.com>
2018-02-01 16:32:18 +0100

    Add test for first check in

---
+++ tdt4140-gr1875/app.core/src/test/java/tdt4140/gr1875/app/core/CheckInTest.java
@@ -0,0 +1,14 @@
+package tdt4140.gr1875.app.core;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CheckInTest {
+	
+	@Test
+	public void exampleTest() {
+		Assert.assertTrue(true);
+	}
+
+}
