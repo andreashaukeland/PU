36727f8249ca4814fb1d2c99933a909f1b171d46
Peder Breivik Grundvold <pederbg@stud.ntnu.no>
2018-02-28 14:28:57 +0100

    Add help function to read from database

---
+++ tdt4140-gr1875/app.core/src/main/java/tdt4140/gr1875/app/core/ReadSQL.java
@@ -0,0 +1,68 @@
+package tdt4140.gr1875.app.core;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import com.mysql.cj.jdbc.result.ResultSetMetaData;
+
+public class ReadSQL {
+	
+	public static ArrayList<ArrayList<String>> getFromDB(String query) {
+		
+		Connection conn = connectDB();
+		
+		Statement stmt = null;
+		ResultSet rs = null;
+		ArrayList<ArrayList<String>> table = null;
+		
+		System.out.println("Running query.." + "\n");
+		
+		try {
+		    stmt = conn.createStatement();
+		    rs = stmt.executeQuery(query);
+		    java.sql.ResultSetMetaData rsmd = rs.getMetaData();
+
+		    int colNum = rsmd.getColumnCount();
+		    table = new ArrayList<ArrayList<String>>();
+		    
+	        while(rs.next()) {
+	        	//System.out.println(rs.getString("fornavn") + " " + rs.getString("etternavn"));
+	        	ArrayList<String> col = new ArrayList<String>();
+	        	for (int i = 1; i < colNum+1; i++) {
+					col.add(rs.getString(i));
+				}
+	        	table.add(col);
+		    }
+
+
+		}
+		catch (SQLException ex){
+		    System.out.println("SQLException: " + ex.getMessage());
+		    System.out.println("SQLState: " + ex.getSQLState());
+		    System.out.println("VendorError: " + ex.getErrorCode());
+		}
+
+	return table;
+	}
+	
+	private static Connection connectDB() {
+		System.out.println("Connecting to database..." + "\n");
+		
+		Connection conn = null;
+		try {
+		    conn =
+		       DriverManager.getConnection("jdbc:mysql://mysql.stud.ntnu.no/martisku_db","martisku_pu","pu75");
+
+		} catch (SQLException ex) {
+		    System.out.println("SQLException: " + ex.getMessage());
+		    System.out.println("SQLState: " + ex.getSQLState());
+		    System.out.println("VendorError: " + ex.getErrorCode());
+		}
+		return conn;
+	}
+
+}
---
+++ tdt4140-gr1875/app.core/src/main/java/tdt4140/gr1875/app/core/test2.java
@@ -0,0 +1,8 @@
+package tdt4140.gr1875.app.core;
+
+//Example function:
+public class test2 {
+	public static void main(String[] args) {
+		System.out.println(ReadSQL.getFromDB("SELECT Fornavn FROM runner"));
+	}
+}
