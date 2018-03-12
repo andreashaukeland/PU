7bab9f686886b6328f2e0fb41c6fde4c8c5d9d3b
Henrik <henrikmaba@gmail.com>
2018-03-12 12:59:02 +0100

    Issue #23 - small refactoring

--- tdt4140-gr1875/app.core/src/test/java/tdt4140/gr1875/app/core/UseDBTest.java
+++ tdt4140-gr1875/app.core/src/test/java/tdt4140/gr1875/app/core/UseDBTest.java
@@ -15,11 +15,12 @@ public class UseDBTest {
 	/*
 	 * The methods getFreeID() and connectDB() are being used (and tested) by other methods. 
 	 */
-	int id = UseDB.getFreeID("training");
+	
+	//int id = UseDB.getFreeID("training"); //used as id if getIDByName is not working
 
 	@Test
 	public void DBTest() {
-		UseDB.addRow("training", id, "TestTrack", "12:00", "2019-03-12", 0);
+		UseDB.addRow("training", UseDB.getFreeID("training"), "TestTrack", "12:00", "2019-03-12", 0);
 		
 		ArrayList<ArrayList<String>> result1 = UseDB.getTable("SELECT place FROM training");
 		String track = result1.get(result1.size()-1).get(0);
@@ -27,7 +28,7 @@ public class UseDBTest {
 		Assert.assertEquals("TestTrack", track);
 		//Assert.assertEquals("true", "true"); // used to test when ntnu database is down
 		
-		UseDB.deleteRow("training", id);
+		UseDB.deleteRow("training", Integer.parseInt(UseDB.getIDByName("training", "place=TestTrack").get(0).get(0)));
 	}
 
 
