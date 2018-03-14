caca3679f93011f17292b4ce8acc4e6e66615466
Henrik <henrikmaba@gmail.com>
2018-03-14 12:58:21 +0100

    Issue #23 - copy from trainer to training
    not finnished

---
+++ tdt4140-gr1875/app.ui/src/main/java/tdt4140/gr1875/app/ui/TrainingMainScreenController.java
@@ -0,0 +1,90 @@
+package tdt4140.gr1875.app.ui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXDrawer.DrawerDirection;
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerSlideCloseTransition;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+public class TrainingMainScreenController implements Initializable{
+
+	
+	private JFXDrawer drawer;
+	
+	@FXML
+	private JFXHamburger hamburger;
+	
+	@FXML
+	private StackPane stackPane;
+	
+	@FXML
+	private BorderPane borderPane;
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		initDrawer();
+	}
+	
+	
+	private void initDrawer() {
+		drawer = new JFXDrawer();
+		drawer.setDirection(DrawerDirection.RIGHT);
+		drawer.setDefaultDrawerSize(100);
+		VBox toolbar;
+		try {
+			toolbar = FXMLLoader.load(getClass().getResource("TrainerToolbar.fxml"));
+			drawer.setSidePane(toolbar);
+			
+			HamburgerSlideCloseTransition task = new HamburgerSlideCloseTransition(hamburger);
+			task.setRate(-1);
+			hamburger.addEventHandler(MouseEvent.MOUSE_CLICKED, (Event e) -> {
+					task.setRate(task.getRate() * -1);
+					task.play();
+					
+					if (drawer.isShown()) {
+						borderPane.setRight(null);
+						drawer.close();
+					}
+					else {
+						borderPane.setRight(drawer);
+						drawer.open();;
+						
+					}
+			});
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+	
+	
+}
