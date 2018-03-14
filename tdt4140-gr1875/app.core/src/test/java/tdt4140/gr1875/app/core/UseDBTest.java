11f94839ff8947cf52642d011d025bc8838bad83
Henrik <henrikmaba@gmail.com>
2018-03-14 14:32:27 +0100

    Issue #23 - dbtest update v2

--- tdt4140-gr1875/app.core/src/test/java/tdt4140/gr1875/app/core/UseDBTest.java
+++ tdt4140-gr1875/app.core/src/test/java/tdt4140/gr1875/app/core/UseDBTest.java
@@ -15,19 +15,20 @@ public class UseDBTest {
 	/*
 	 * The methods getFreeID() and connectDB() are being used (and tested) by other methods. 
 	 */
-	int id = UseDB.getFreeID("training");
+	
+	int id = UseDB.getFreeID("training"); //used as id if getIDByName is not working
 
 	@Test
 	public void DBTest() {
 		UseDB.addRow("training", id, "TestTrack", "12:00", "2019-03-12", 0);
 		
-		ArrayList<ArrayList<String>> result1 = UseDB.getTable("SELECT place FROM training");
+		ArrayList<ArrayList<String>> result1 = UseDB.getTable("SELECT place FROM training WHERE trainingid="+id);
 		String track = result1.get(result1.size()-1).get(0);
 			
 		Assert.assertEquals("TestTrack", track);
 		//Assert.assertEquals("true", "true"); // used to test when ntnu database is down
 		
-		UseDB.deleteRow("training", id);
+		UseDB.deleteRow("training", Integer.parseInt(UseDB.getIDByName("training", "place=TestTrack").get(0).get(0)));
 	}
 
 
@@ -43,3 +44,4 @@ public class UseDBTest {
 
 
 }
+
