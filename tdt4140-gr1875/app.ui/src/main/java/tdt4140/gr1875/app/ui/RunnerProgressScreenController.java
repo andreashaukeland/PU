812669548e11b673bbd6bb08d7af2867d5debe90
Unknown <martin.skurtveit@gmail.com>
2018-03-21 14:42:37 +0100

    Issue #21 - add comment to results

--- tdt4140-gr1875/app.ui/src/main/java/tdt4140/gr1875/app/ui/RunnerProgressScreenController.java
+++ tdt4140-gr1875/app.ui/src/main/java/tdt4140/gr1875/app/ui/RunnerProgressScreenController.java
@@ -31,6 +31,7 @@ import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
@@ -71,6 +72,7 @@ public class RunnerProgressScreenController implements Initializable{
 	@FXML private Label labelMobile;
 	@FXML private Label labelEmail;
 	@FXML private Label labelAge;
+	@FXML private TextField commentTextfield;
 	
 	
 	private RunnerProgressScreen runnerProgressScreen = new RunnerProgressScreen();
@@ -88,10 +90,16 @@ public class RunnerProgressScreenController implements Initializable{
 		}
 		initDrawer();
 		initCol();
-		ArrayList<ArrayList<String>> list = UseDB.getTable("SELECT training.trainingid, place, date, distance, result.time"
+		ArrayList<ArrayList<String>> list = UseDB.getTable("SELECT training.trainingid, place, date, distance, result.time, result.comment"
 				+ " FROM training join result on result.trainingid = training.trainingid join runner on runner.runnerid = result.runnerid"
 				+ " WHERE runner.runnerid =" + currentUser +";");
 		tableView.getItems().setAll(getResults(list));
+		tableView.getSelectionModel().selectedItemProperty().addListener((ob, oldval, newval)->{
+			if (newval != null)
+				commentTextfield.setText(newval.getComment());
+			else
+				commentTextfield.setText("No comment given");
+	});
 		initProgressChart();
 		initInformationTab();
 	}
@@ -213,11 +221,12 @@ public class RunnerProgressScreenController implements Initializable{
 		ArrayList<Results> results = new ArrayList<>();		
 		for (int i = 0; i < list.size(); i++) {
 			ArrayList<String> indexedlist = list.get(i);
-			results.add(new Results(indexedlist.get(0), indexedlist.get(1), indexedlist.get(2),indexedlist.get(3),indexedlist.get(4)));
+			results.add(new Results(indexedlist.get(0), indexedlist.get(1), indexedlist.get(2),indexedlist.get(3),indexedlist.get(4), indexedlist.get(5)));
 		}
 		return FXCollections.observableArrayList(results);
 	}
 
+	
 	
 	public static class Results{
 		private final SimpleStringProperty trainingNumber;
@@ -225,14 +234,16 @@ public class RunnerProgressScreenController implements Initializable{
 		private final SimpleStringProperty trainingDate;
 		private final SimpleStringProperty time;
 		private final SimpleStringProperty distance;
+		private final SimpleStringProperty comment;
 
 		
-		public Results(String trainingNumber, String trainingPlace, String trainingDate, String distance, String time) {
+		public Results(String trainingNumber, String trainingPlace, String trainingDate, String distance, String time, String comment) {
 			this.trainingNumber = new SimpleStringProperty(trainingNumber);
 			this.trainingPlace = new SimpleStringProperty(trainingPlace);
 			this.trainingDate = new SimpleStringProperty(trainingDate);
 			this.time = new SimpleStringProperty(time);
 			this.distance = new SimpleStringProperty(distance);
+			this.comment = new SimpleStringProperty(comment);
 		}
 		
 		public String getTrainingNumber() {
@@ -252,7 +263,36 @@ public class RunnerProgressScreenController implements Initializable{
 		public String getTime() {
 			return time.get();
 		}
+		public String getComment() {
+			return comment.get();
+		}
 		
+	}
+
+	private void updateComment() {
+		tableView.getSelectionModel().selectedItemProperty().addListener((ob, oldval, newval)->{
+			if (newval != null)
+				commentTextfield.setText(newval.getComment());
+			else
+				commentTextfield.setText("No comment given");
+	});
+	}
+	public void onSubmit() {
+		String comment = commentTextfield.getText();
+		boolean submitted = .submitTime(SessionInformation.userId, comment);
+		if(! submitted) {
+			Alert alert = new Alert(Alert.AlertType.INFORMATION);
+			alert.setHeaderText(null);
+			alert.setContentText("Could not submit to database");
+			alert.showAndWait();
+		}
+		else {
+			commentTextfield.clear();
+			Alert alert = new Alert(Alert.AlertType.INFORMATION);
+			alert.setHeaderText(null);
+			alert.setContentText("Comment updated");
+			alert.showAndWait();
+		}
 	}
 
 }
